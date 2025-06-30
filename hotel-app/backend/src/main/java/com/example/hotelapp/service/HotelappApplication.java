c46a65f053423611f1fb21dd73b6242f85015ed0
Franklin <franklinvelsquezhorna@gmail.com>
2025-06-30 02:13:15 -0500

    Proyecto actualizado 7

---
+++ hotel-app/backend/src/main/java/com/example/hotelapp/service/HotelappApplication.java
@@ -0,0 +1,12 @@
+package com.example.hotelapp.service;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelappApplication.class, args);
+	}
+
+}   
